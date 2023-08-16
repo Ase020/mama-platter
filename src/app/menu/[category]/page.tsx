984efb381068a953f4c35d475913e7197f702b62
felixNyalenda <felixasejunior@gmail.com>
2023-08-16 14:20:43 +0300

    menu items build

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,40 @@
-type Props = {};
+import { pizzas } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
 
-const CategoryPage = (props: Props) => {
-  return <div>CategoryPage</div>;
+const CategoryPage = () => {
+  return (
+    <section className="flex flex-wrap text-red-500">
+      {pizzas.map((pizza) => (
+        <Link
+          className="h-[60vh] p-4 w-full sm:w-1/2 lg:w-1/3 border-red-400 border-r-2 border-b-2 flex flex-col justify-between group even:bg-fuchsia-50"
+          href={`/product/${pizza.id}`}
+          key={pizza.id}
+        >
+          {pizza.img && (
+            <div className="relative h-[80%]  hover:rotate-[-60deg] transition-all duration-500 ease-in">
+              <Image
+                src={pizza.img}
+                alt={pizza.title}
+                fill
+                className="object-contain"
+              />
+            </div>
+          )}
+
+          <div className="flex items-center justify-between font-bold group">
+            <h1 className="text-lg capitalize p-2 ">{pizza.title}</h1>
+            <h2 className="text-base group-hover:hidden">
+              Ksh.{Math.floor(pizza.price * 52).toLocaleString("en-US")}
+            </h2>
+            <button className="bg-red-500 text-white text-sm p-2 rounded-md uppercase hidden group-hover:block">
+              add to cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </section>
+  );
 };
 
 export default CategoryPage;
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Menu = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <section className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((item) => (
         <Link
           key={item.id}
@@ -24,7 +24,7 @@ const Menu = () => {
           </div>
         </Link>
       ))}
-    </div>
+    </section>
   );
 };
 
